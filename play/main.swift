84c24109805b15766840d8418408f8938a79a611
sebprest <38880204+sebprest@users.noreply.github.com>
2020-04-09 10:22:55 +0100

    Implement help message

--- play/main.swift
+++ play/main.swift
@@ -8,5 +8,17 @@
 
 import Foundation
 
-print("Hello, World!")
+let userArgs = Array(ProcessInfo.processInfo.arguments.dropFirst())
 
+// print the help message when appropriate
+if userArgs.count == 0 || userArgs.contains(where: { $0 == "--help" || $0 == "-h" }) {
+  print(
+    """
+    Usage: play [options] [file]
+
+    Options:
+      --help | -h:
+        Print this message.
+    """)
+  exit(0)
+}
