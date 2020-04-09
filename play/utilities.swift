5aaeac2b93d139d74ef2eb143d2f0c82bc608cfa
sebprest <38880204+sebprest@users.noreply.github.com>
2020-04-09 18:26:55 +0100

    Add playback control key config

---
+++ play/utilities.swift
@@ -0,0 +1,16 @@
+//
+//  util.swift
+//  play
+//
+//  Created by sebprest on 09/04/2020.
+//  Copyright © 2020 sebprest. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static let PLAY_PAUSE_KEY = "p"
+    static let SEEK_BACKWARD_KEY = "["
+    static let SEEK_FORWARD_KEY = "]"
+    static let SEEK_AMOUNT = 5.0
+}
